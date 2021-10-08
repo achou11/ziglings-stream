79fcb1fcfd8ec341f6452aac33c7bbb9853cac6b
achou11 <andrewchou@fastmail.com>
2021-10-08 16:12:24 -0400

    Complete 037

--- exercises/037_structs.zig
+++ exercises/037_structs.zig
@@ -36,6 +36,7 @@ const Character = struct {
     class: Class,
     gold: u32,
     experience: u32,
+    health: u8,
 };
 
 pub fn main() void {
@@ -44,6 +45,7 @@ pub fn main() void {
         .class = Class.wizard,
         .gold = 20,
         .experience = 10,
+        .health = 100,
     };
 
     // Glorp gains some gold.
